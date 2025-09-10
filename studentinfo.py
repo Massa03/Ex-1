b1c32979f4529ee9ec0c7a3dd32235cb487ba7cf
massabelal <93160778+massabelal@users.noreply.github.com>
2025-09-09 21:28:44 -0400

    Submit RESTful API Python

---
+++ studentinfo.py
@@ -0,0 +1,25 @@
+import requests
+
+# API endpoint
+url = "https://student-info-api.netlify.app/.netlify/functions/submit_student_info"
+
+# Your info in JSON format
+data = {
+    "UCID": "mb287",
+    "first_name": "Massa",
+    "last_name": "Belal",
+    "github_username": "Massa03",
+    "discord_username": "sea018355",
+    "favorite_cartoon": "Tom and Jerry",
+    "favorite_language": "Python",
+    "movie_or_game_or_book": "La La Land",
+    "section": "103"
+}
+
+try:
+    response = requests.post(url, json=data)
+    print("Status:", response.status_code)
+    print("Response:", response.text)
+
+except requests.exceptions.RequestException as e:
+    print("Error:", e)
